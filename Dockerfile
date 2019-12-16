d908d9931a7b2b2e13c9dc439a65b6121f3553d0
Victor Mesquita <mesquitadev@gmail.com>
2019-12-16 11:59:40 -0300

    D- Models - Creating a ACL Models

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM php:7.3.6-fpm-alpine3.9
+MAINTAINER Victor Mesquita <mesquitadev@gmail.com>
 RUN apk add --no-cache openssl bash nodejs npm postgresql-dev
 RUN docker-php-ext-install bcmath pdo pdo_pgsql
 WORKDIR /var/www
@@ -11,6 +12,9 @@ RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local
 COPY . /var/www
 
 RUN chmod -R 777 /var/www/storage
+
+RUN adduser -D laravel
+USER laravel
 
 EXPOSE 9000
 ENTRYPOINT ["php-fpm"]
---
+++ database/migrations/2019_12_16_141633_create_role_permissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('role_id', false, true);
+            $table->string('object', 55);
+            $table->string('operation', 55);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+}
